3a4fd88aa9e4c813999341a7f16c69692943ba7d
ialvarez <ignacioalvarez2854@gmail.com>
2026-02-26 17:53:02 +0100

    Updated deploy config

--- pipelines/common-steps/deploy.sh
+++ pipelines/common-steps/deploy.sh
@@ -2,4 +2,4 @@
 
 set -x
 du -hs * | sort -h
-sam deploy --template-file template.yaml --config-env ${ENVIRONMENT} --resolve-s3 --force-upload --no-fail-on-empty-changeset --no-progressbar
+sam deploy --template-file template.yaml --config-env ${ENVIRONMENT} --resolve-s3 --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar
